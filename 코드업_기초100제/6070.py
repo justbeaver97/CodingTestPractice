829ff89f449b0e6fda137dcbe1094a28b359a3bc
justbeaver97 <73840274+justbeaver97@users.noreply.github.com>
2021-04-14 22:49:17 +0900

    0414_2

---
+++ 코드업_기초100제/6070.py
@@ -0,0 +1,10 @@
+input = int(input())
+
+if input==12 or input==1 or input==2:
+    print("winter")
+elif input==3 or input==4 or input==5:
+    print("spring")
+elif input==6 or input==7 or input==8:
+    print("summer")
+else:
+    print("fall")
